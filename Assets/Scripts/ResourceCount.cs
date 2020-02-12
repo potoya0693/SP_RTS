909f7a941f5c1f0638dc40d2dd245c41b5c14e6c
Alan Hsu <alander1234@users.noreply.github.com>
2020-02-11 23:51:22 -0800

    UI

---
+++ Assets/Scripts/ResourceCount.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using RTS;
+
+public class ResourceCount : MonoBehaviour
+{
+      public string key;
+
+        public Text textLabel;
+
+        // Update is called once per frame
+        void Update()
+        {
+            textLabel.text = PlayerPrefs.GetInt(key, 0).ToString();
+        }
+}
---
+++ Assets/Scripts/gamemanager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gamemanager : MonoBehaviour
+{
+    private void Start()
+    {
+        PlayerPrefs.SetInt("Gold", 500);
+        PlayerPrefs.SetInt("Lumber", 100);
+        PlayerPrefs.SetInt("Food", 100);
+    }
+}
